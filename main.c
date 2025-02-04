7f784e37493608205ea086b405525549c7f96eab
guillen-porto <guillen.porto.gonzalez@udc.es>
2025-02-04 18:24:07 +0100

    Implementado funcionamiento de los leds

--- main.c
+++ main.c
@@ -15,26 +15,46 @@ void delay(void)
 void led_green_init()
 {
   // SIM->COPC
+  SIM->COPC |= SIM_COPC_COPT(0); //Se puede poner directamente SIM->COPC |= 0
+
   // SIM->SCGC5
+  //SIM->SCGC5 |= SIM_SCGC5_PORTD(1);
+  SIM->SCGC5 |= SIM_SCGC5_PORTD(1);
+
   // PORTD->PCR[5]
+  PORTD->PCR[5] |= PORT_PCR_MUX(1);
+
   // GPIOD->PDDR
+  GPIOD->PDDR |= GPIO_PDDR_PDD(1 << 5);
+
   // GPIOD->PSOR
+  GPIOD->PSOR |= GPIO_PSOR_PTSO(1 << 5);
 }
 
 void led_green_toggle()
 {
   //
+    GPIOD->PTOR |= GPIO_PTOR_PTTO(1 << 5);
 }
 
 // LED_RED = PTE29
 void led_red_init()
 {
-  //
+
+  //SIM->SCGC5 |= SIM_SCGC5_PORTE(1);
+  SIM->SCGC5 |= SIM_SCGC5_PORTE(1);
+
+  PORTE->PCR[29] |= PORT_PCR_MUX(1);
+
+  GPIOE->PDDR |= GPIO_PDDR_PDD(1 << 29);
+
+  GPIOE->PSOR |= GPIO_PSOR_PTSO(1 << 29);
 }
 
 void led_red_toggle(void)
 {
   //
+  GPIOE->PTOR |= GPIO_PTOR_PTTO(1 << 29);
 }
 
 int main(void)
@@ -44,8 +64,10 @@ int main(void)
 
   while (1) {
     led_green_toggle();
+    //Inicialmente delay aquí
     delay();
     led_red_toggle();
+    //delay();
   }
 
   return 0;
---
+++ makefile
@@ -0,0 +1,18 @@
+main.elf:
+	arm-none-eabi-gcc -I ./includes -O2 -Wall -mthumb -mcpu=cortex-m0plus -c -o startup.o startup.c
+	
+	arm-none-eabi-gcc -I ./includes -O2 -Wall -mthumb -mcpu=cortex-m0plus -c -o main.o main.c
+
+	arm-none-eabi-gcc -O2 -Wall -mthumb -mcpu=cortex-m0plus --specs=nano.specs -Wl,--gc-sections,-Map=main.map -T link.ld main.o startup.o -o main.elf
+
+
+flash: main.elf
+	openocd -f openocd.cfg -c "program main.elf verify reset exit"
+
+
+clean: 
+	$(RM) *.o
+
+
+mrproper:
+	$(RM) *.o main.elf
