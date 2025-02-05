c0158c24e457bde5a70948bcc29bc73e5c7e81e7
guillen-porto <guillen.porto.gonzalez@udc.es>
2025-02-05 11:40:01 +0100

    Botones inicializados

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ void led_green_init()
   PORTD->PCR[5] |= PORT_PCR_MUX(1);
 
   // GPIOD->PDDR
-  GPIOD->PDDR |= GPIO_PDDR_PDD(1 << 5);
+  GPIOD->PDDR |= GPIO_PDDR_PDD(1 << 5); //Hace que el pin 5 del puerto D sea de salida (1)
 
   // GPIOD->PSOR
   GPIOD->PSOR |= GPIO_PSOR_PTSO(1 << 5);
@@ -57,10 +57,31 @@ void led_red_toggle(void)
   GPIOE->PTOR |= GPIO_PTOR_PTTO(1 << 29);
 }
 
+//SWITCH1= PTC3
+void switch1_init(void){
+    SIM->SCGC5 |= SIM_SCGC5_PORTC(1);
+
+    PORTC->PCR[3] |= PORT_PCR_MUX(1);
+
+    GPIOC->PDDR |= GPIO_PDDR_PDD(0 << 3); //Hace que el pin 3 del puerto C sea de entrada (0)
+
+}
+
+//SWITCH3= PTC12
+void switch3_init(void){
+    SIM->SCGC5 |= SIM_SCGC5_PORTC(1); //entiendo que solo hace falta inicializarlo una vez (en button_green_init)
+
+    PORTC->PCR[3] |= PORT_PCR_MUX(1);
+
+    GPIOC->PDDR |= GPIO_PDDR_PDD(0 << 12); //Hace que el pin 3 del puerto C sea de entrada (0)
+}
+
 int main(void)
 {
   led_green_init();
   led_red_init();
+  button_green_init();
+  button_red_init();
 
   while (1) {
     led_green_toggle();
